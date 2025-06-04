8e36f584011319af82dc5f816e6657de5b04bf0c
JHONY ALEJANDRO PEREZ GOMEZ <jhony.1701521539@ucaldas.edu.co>
2025-06-03 21:03:38 -0500

    Refactor QNodes strategy and update main entry point for improved network analysis

--- src/main.py
+++ src/main.py
@@ -1,19 +1,20 @@
 from src.controllers.strategies.force import BruteForce
 from src.controllers.manager import Manager
+from src.controllers.strategies.q_nodes import QNodes
 
 
 def iniciar():
     """Punto de entrada principal"""
                    # ABCD #
-    estado_inicio = "1000"
-    condiciones =   "1110"
-    alcance =       "1110"
-    mecanismo =     "1110"
+    estado_inicio = "100000000000000"
+    condiciones =   "111111111111111"
+    alcance =       "111111111111110"
+    mecanismo =     "111111111111111"
 
     config_sistema = Manager(estado_inicial=estado_inicio)
 
     ### Ejemplo de solución mediante módulo de fuerza bruta ###
-    analizador_fb = BruteForce(config_sistema)
+    analizador_fb =     QNodes(config_sistema)
     sia_uno = analizador_fb.aplicar_estrategia(condiciones, alcance, mecanismo)
 
     print(sia_uno)
